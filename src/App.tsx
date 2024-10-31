668d8df3c2dd7a8af69213e7c0752d074aca9d7b
anakinfd886 <anakin.fd886@gmail.com>
2024-10-31 22:57:04 +0800

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ const client = generateClient<Schema>();
 
 function App() {
 
-  const { signOut } = useAuthenticator();
+  //const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -29,7 +30,8 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      {/* <h1>My todos</h1> */}
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
